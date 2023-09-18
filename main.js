6f88217c627d21e7fbc51ec952e81a2048d498e7
kpoper00 <114153584+kpoper00@users.noreply.github.com>
2023-09-18 12:41:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("seja bem-vindo")
+function pom() {
+    alert ("botão pom clicando");
+}
+document.querySelector('.tecla_pom').onclick = pom;
+
+function clap() {
+    alert("botão clap clicando")
+}
+document.querySelector('.tecla_clap').onclick = clap;
